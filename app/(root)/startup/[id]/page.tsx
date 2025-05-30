cff7c75affb0f9a2eb66d11b117ec80d0fad54ef
adovg <kroatoan@i.ua>
2025-05-30 23:19:17 +0300

    make parallel reauest

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -18,11 +18,18 @@ const md = markdownit();
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+  const [post, { select: editorPosts }] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: "editor-picks-new",
+    }),
+  ]);
 
-  const { select: editorPosts } = await client.fetch(PLAYLIST_BY_SLUG_QUERY, {
-    slug: "editor-picks-new",
-  });
+  // const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
+
+  // const { select: editorPosts } = await client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+  //   slug: "editor-picks-new",
+  // });
 
   //if (!post?.title) return <div>Title is missing</div>;
 
