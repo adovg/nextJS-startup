a1a1cf216fd610ed1b09ec3c67a1373a2578db11
adovg <kroatoan@i.ua>
2025-06-04 19:56:11 +0300

    fix font import

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,53 +1,53 @@
-import { LocalFont } from 'next/font/local';
+import localFont from "next/font/local";
 
-export const workSans = LocalFont({
+export const workSans = localFont({
   src: [
     {
-      path: '../public/fonts/WorkSans-Thin.ttf',
-      weight: '100',
-      style: 'normal',
+      path: "../public/fonts/WorkSans-Thin.ttf",
+      weight: "100",
+      style: "normal",
     },
     {
-      path: '../public/fonts/WorkSans-ExtraLight.ttf',
-      weight: '200',
-      style: 'normal',
+      path: "../public/fonts/WorkSans-ExtraLight.ttf",
+      weight: "200",
+      style: "normal",
     },
     {
-      path: '../public/fonts/WorkSans-Light.ttf',
-      weight: '300',
-      style: 'normal',
+      path: "../public/fonts/WorkSans-Light.ttf",
+      weight: "300",
+      style: "normal",
     },
     {
-      path: '../public/fonts/WorkSans-Regular.ttf',
-      weight: '400',
-      style: 'normal',
+      path: "../public/fonts/WorkSans-Regular.ttf",
+      weight: "400",
+      style: "normal",
     },
     {
-      path: '../public/fonts/WorkSans-Medium.ttf',
-      weight: '500',
-      style: 'normal',
+      path: "../public/fonts/WorkSans-Medium.ttf",
+      weight: "500",
+      style: "normal",
     },
     {
-      path: '../public/fonts/WorkSans-SemiBold.ttf',
-      weight: '600',
-      style: 'normal',
+      path: "../public/fonts/WorkSans-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
     },
     {
-      path: '../public/fonts/WorkSans-Bold.ttf',
-      weight: '700',
-      style: 'normal',
+      path: "../public/fonts/WorkSans-Bold.ttf",
+      weight: "700",
+      style: "normal",
     },
     {
-      path: '../public/fonts/WorkSans-ExtraBold.ttf',
-      weight: '800',
-      style: 'normal',
+      path: "../public/fonts/WorkSans-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
     },
     {
-      path: '../public/fonts/WorkSans-Black.ttf',
-      weight: '900',
-      style: 'normal',
+      path: "../public/fonts/WorkSans-Black.ttf",
+      weight: "900",
+      style: "normal",
     },
   ],
-  variable: '--font-work-sans',
-  display: 'swap',
+  variable: "--font-work-sans",
+  display: "swap",
 });
